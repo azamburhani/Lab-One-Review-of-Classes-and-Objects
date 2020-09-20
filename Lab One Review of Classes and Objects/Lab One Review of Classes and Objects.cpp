e8106b1115bfae4ce847377435b744c2e6a66174
azamburhani <60987466+azamburhani@users.noreply.github.com>
2020-09-19 23:00:40 -0400

    Add project files.

---
+++ Lab One Review of Classes and Objects/Lab One Review of Classes and Objects.cpp
@@ -0,0 +1,22 @@
+/*
+
+	Name : Ahmad Azam (100698308), Ryan Maku ()
+
+	Date : September 19, 2020
+
+	Description : Lab One Review of Classes and Objects. Creating a programe that stores detial about work ticket. first it stores the input but before storing eac data the program will
+	 runs it throgh validation and than stored into correct variable.
+
+
+*/
+
+
+#include<iostream>
+#include <stdexcept>
+#include<string>
+
+using namespace std;
+
+class WorkTicket							// main class name
+{
+}
